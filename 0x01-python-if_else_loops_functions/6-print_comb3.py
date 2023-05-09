fa22ca1a4bd7ebae930dc3b9f4b9b7cbf9ba111a
Donphili <donphilip65@yahoo.com>
2023-05-09 14:59:43 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for j in range(1, 90):
-    if j / 10 > j % 10:
+for i in range(1, 90):
+    if i / 10 > j % 10:
         continue
     else:
-        if (j != 89):
-            print("{:02}".format(j), end=", ")
+        if (i != 89):
+            print("{:02}".format(i), end=", ")
         else:
-            print("{}".format(j))
+            print("{}".format(i))
