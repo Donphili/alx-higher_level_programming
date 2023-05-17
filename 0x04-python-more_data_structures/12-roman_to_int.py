1938b8a5f2f1bc96d2a9c630d4ba960dc3df3bee
Donphili <donphilip65@yahoo.com>
2023-05-17 22:24:26 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    for x in list(a_dictionary):
-        if a_dictionary[x] == value:
-            del a_dictionary[x]
-    return (a_dictionary)
+
+def to_subtract(list_num):
+to_sub = 0
+max_list = max(list_num)
+for n in list_num:
+if max_list > n:
+to_sub += n
+return (max_list - to_sub)
