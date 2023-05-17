1059e985c12fdb6bda7c08272371012be14c129b
Donphili <donphilip65@yahoo.com>
2023-05-17 23:02:15 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-
-def to_subtract(list_num):
-to_sub = 0
-max_list = max(list_num)
-for n in list_num:
-if max_list > n:
-to_sub += n
-return (max_list - to_sub)
+def roman_to_int(roman_string):
+    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5,
+             'I': 1, 'Z': 0}
+    total = 0
+    if (isinstance(roman_string, str)):
+        i = 0
+        while i < len(roman_string):
+            f = roman_string[i]
+            if (i + 1) < len(roman_string):
+                n = roman_string[i + 1]
+            else:
+                n = 'Z'
+            if int(roman[f]) < int(roman[n]) and n != 'Z':
+                total += (int(roman[n]) - int(roman[f]))
+                i += 2
+            else:
+                total += int(roman[f])
+                i += 1
+        return(total)
+    return(0)
