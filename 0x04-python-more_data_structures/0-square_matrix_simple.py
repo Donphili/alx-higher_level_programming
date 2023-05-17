4e708f7364a47b95d71c2355c1913c9f0f2b4c2e
Donphili <donphilip65@yahoo.com>
2023-05-17 09:34:28 -0700

    updated task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,8 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     new_row = []
-    for j in range(len(matrix)):
-        for i in range(len(matrix[i])):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             new_row.append(matrix[i][j] ** 2)
         new_matrix.append(new_row)
         new_row = []
