50deb1520c0eb265102b3114efc42f906c362031
Donphili <donphilip65@yahoo.com>
2023-05-17 16:33:00 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,14 @@
 from sys import argv
 
 def principal():
-    print('{} argument'.format(len(argv) - 1), end='')
-    if len(argv) == 1:
-        print('s.')
-    elif len(argv) == 2:
-        print(':')
-    else:
-        print('s:')
-    for i in range(1, len(argv)):
-        print('{}: {}'.format(i, argv[i]))
-
-
+print('{} argument'.format(len(argv) - 1), end='')
+if len(argv) == 1:
+print('s.')
+elif len(argv) == 2:
+print(':')
+else:
+print('s:')
+for i in range(1, len(argv)):
+print('{}: {}'.format(i, argv[i]))
 if __name__ == "__main__":
-    principal()
+principal()
