e1d7a38771b7c4b0158d130ef5f870d1ee8e07c3
Donphili <donphilip65@yahoo.com>
2023-06-19 16:49:43 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,11 +2,22 @@
 """
 File: rectangle.py
 Desc: This module contains a class; Rectangle
-Author: Onyejekwe Philip(Donphili)
-Date Created: 19 jun 2023
 """
 
 from models.base import Base
+
+
+def check_value(name, value, sides=True):
+    """
+    Checks if correct input was inserted
+    """
+    if not isinstance(value, int):
+        raise TypeError("{} must be an integer".format(name))
+    if sides:
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+    elif value < 0:
+        raise ValueError("{} must be >= 0".format(name))
 
 
 class Rectangle(Base):
@@ -17,6 +28,10 @@ class Rectangle(Base):
         """
         Initialization of the object
         """
+        self._width = None
+        self._height = None
+        self._x = None
+        self._y = None
         self.width = width
         self.height = height
         self.x = x
@@ -27,133 +42,80 @@ class Rectangle(Base):
         """
         String representation of class
         """
-        return ("[Rectangle] (" +
-                str(self.id) + ") " + str(self.__x) + "/" + str(self.__y) +
-                " - " + str(self.__width) + "/" + str(self.__height))
-
-    def check_value(self, name, value, sides=True):
-        """
-        Checks if correct input was inserted
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if sides:
-            if value <= 0:
-                raise ValueError("{} must be > 0".format(name))
-        elif value < 0:
-            raise ValueError("{} must be >= 0".format(name))
+        return (
+            f"[Rectangle] ({self.id}) {self._x}/{self._y} - {self._width}/{self._height}"
+        )
 
     @property
     def width(self):
         """
         getter for width
         """
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
         """
         Sets the value for width
         """
-        self.check_value('width', value)
-        self.__width = value
+        check_value("width", value)
+        self._width = value
 
     @property
     def height(self):
         """
         Getter for height
         """
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, value):
         """
         Sets the value for height
         """
-        self.check_value('height', value)
-        self.__height = value
+        check_value("height", value)
+        self._height = value
 
     @property
     def x(self):
         """
         Getter for x
         """
-        return self.__x
+        return self._x
 
     @x.setter
     def x(self, value):
         """
         Sets the value for x
         """
-        self.check_value('x', value, False)
-        self.__x = value
+        check_value("x", value, False)
+        self._x = value
 
     @property
     def y(self):
         """
         Getter for y
         """
-        return self.__y
+        return self._y
 
     @y.setter
     def y(self, value):
         """
-        Sets the value fo y
+        Sets the value for y
         """
-        self.check_value('y', value, False)
-        self.__y = value
+        check_value("y", value, False)
+        self._y = value
 
     def area(self):
         """
         Returns the area of the rectangle
         """
-        return (self.__width * self.__height)
+        return self._width * self._height
 
     def display(self):
         """
         Prints in stdout the Rectangle instance with the character '#'
         """
-        print("\n" * self.__y, end='')
-        for i in range(self.__height):
-            print((' ' * self.__x) + ('#' * self.__width) + '\n', end='')
-
-    def update(self, *args, **kwargs):
-        """
-        Updates the attributes values
-        """
-        if len(args):
-            for n, arg in enumerate(args):
-                if n == 0:
-                    self.id = arg
-                elif n == 1:
-                    self.width = arg
-                elif n == 2:
-                    self.height = arg
-                elif n == 3:
-                    self.x = arg
-                elif n == 4:
-                    self.y = arg
-        else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "width" in kwargs:
-                self.width = kwargs["width"]
-            if "height" in kwargs:
-                self.height = kwargs["height"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
-
-    def to_dictionary(self):
-        """
-        Returns the dictionary representation
-        of the class
-        """
-        d = {}
-        d["id"] = self.id
-        d["width"] = self.width
-        d["height"] = self.height
-        d["x"] = self.x
-        d["y"] = self.y
-        return d
+        print("\n" * self._y, end='')
+        for i in range(self._height):
+            print((' ' * self._x) + ('#' * self._width) + '\n', end='')
